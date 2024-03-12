0e5093902872d5d68f798b259ae470b7a85785cf
Murad <murad.yunus.2017@gmail.com>
2024-03-12 22:37:05 +0400

    SimpleChanges

--- SignalR.WebUI/wwwroot/js/signalr-client.js
+++ SignalR.WebUI/wwwroot/js/signalr-client.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
 
+    const broadcastingMessageToAllClientMethodCall = "BroadcastMessageToAllClients";
+
+    const ReceiveMessageForAllClientsClientMethodCall = "ReceiveMessageForAllClients";
+
+    //Connected with signalr
     const connection = new signalR.HubConnectionBuilder().
         withUrl("/examplehub").
         configureLogging(signalR.LogLevel.Information).
         build();
+
 
     function start() {
         connection.start().then(() => console.log("Connected with signalr"));
@@ -15,5 +21,19 @@ $(document).ready(function () {
     } catch {
         setTimeout(() => start(), 5000);
     }
+
+    //Subscribe to method
+    connection.on(ReceiveMessageForAllClientsClientMethodCall, (message) => {
+        console.log("Incoming message",message);
+    });
+
+    $("#btn-send-message-all-client").click(function () {
+        const message = "hello world";
+
+        connection.invoke(broadcastingMessageToAllClientMethodCall, message).
+            catch(err => console.error("Error"));
+
+
+    });
 
 });
